5128511e716232096c5c56928569a7f17d0c3f6a
wenzhenqian50 <1964310198@qq.com>
2025-09-27 12:38:42 +0800

    V0.1-完成了三环闭合

---
+++ Software/TinyFoc/MDK-ARM/TinyFoc/utils.d
@@ -0,0 +1,46 @@
+tinyfoc/utils.o: ..\User\FOC\utils.c ..\User\FOC\utils.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
+  ..\Core\Inc\stm32f4xx_hal_conf.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_def.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
+  ..\Drivers\CMSIS\Include\core_cm4.h \
+  D:\softs\mdk\ARM\ARMCLANG\include\stdint.h \
+  C:\Users\19643\Desktop\TinyFOC\Software\TinyFoc\Drivers\CMSIS\Include\cmsis_version.h \
+  C:\Users\19643\Desktop\TinyFOC\Software\TinyFoc\Drivers\CMSIS\Include\cmsis_compiler.h \
+  C:\Users\19643\Desktop\TinyFOC\Software\TinyFoc\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Users\19643\Desktop\TinyFOC\Software\TinyFoc\Drivers\CMSIS\Include\mpu_armv7.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  D:\softs\mdk\ARM\ARMCLANG\include\stddef.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_gpio.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_gpio_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_exti.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_dma.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_dma_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_cortex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_adc.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_ll_adc.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_adc_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_flash.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_flash_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_flash_ramfunc.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_i2c.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_i2c_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h \
+  ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h \
+  ..\Middlewares\ST\ARM\DSP\Inc\arm_math.h \
+  ..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  D:\softs\mdk\ARM\ARMCLANG\include\string.h \
+  D:\softs\mdk\ARM\ARMCLANG\include\math.h \
+  D:\softs\mdk\ARM\ARMCLANG\include\float.h \
+  D:\softs\mdk\ARM\ARMCLANG\include\limits.h ..\User\FOC\motor.h \
+  ..\User\MATH\fast_trig.h D:\softs\mdk\ARM\ARMCLANG\include\stdbool.h \
+  ..\User\AS5600\as5600.h ..\Core\Inc\i2c.h ..\Core\Inc\main.h \
+  D:\softs\mdk\ARM\ARMCLANG\include\stdio.h ..\User\FOC\pid.h \
+  ..\User\FOC\foc.h ..\Core\Inc\tim.h
